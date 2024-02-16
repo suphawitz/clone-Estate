f55caedc5038e09722266bb519bed954c7f164c5
Aum Suphawit Jaikaewma <suphawit.aum.si@gmail.com>
2024-02-16 15:06:36 +0700

    slick, add images, fontawsome

---
+++ src/script.js
@@ -0,0 +1,47 @@
+
+// SlickJS 
+$(document).ready(function(){
+
+    // Slider Gallery
+    $('.es-gallery-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+
+    $('.es-gallery-slider .slick-prev').html('<i class="fa-solid fa-angle-left"></i>');
+    $('.es-gallery-slider .slick-next').html('<i class="fa-solid fa-angle-right"></i>');
+
+});
+
+
